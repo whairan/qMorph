55fca2dabac810f9fe5967c742b0bfad38502822
Wali Hairan <whairan@conncoll.edu>
2018-08-21 10:01:00 -0400

    My first commit

--- registration/backend/main.py
+++ registration/backend/main.py
@@ -1,28 +1,8 @@
-from qAutoFit import qAutoFit
-import numpy as np
-from qContours import drawPoints
-import cv2
-import time
-import glob
-import os
 
-from qContours import getUserContourFromImage, getTemplateContourFromImage, createMarkers, drawPoints
-from qContourTools import getError, getFitClosest, guessAffineParameters, computeAffineMatrix, hausdorffDistance, frechetDist, findContourCenter, balloonFit
+from qmorph import *
+from AutoRegister import *
 
-import pandas as pd
-import os.path
-from scipy.interpolate import splprep, splev
-
-from scipy.spatial.distance import directed_hausdorff
-from scipy.optimize import minimize
-from skimage.segmentation import active_contour
-import math
-import sys
-
-
-
-
-	"""
+"""
 	Plan:
 
 	
@@ -33,4 +13,18 @@ import sys
 	and will upload a selected the selected image file into the UPLOADS directory
 	*Create another html button in the templates called REGISTER
 	*Have the registered 
-	"""
+"""
+
+
+InputDir = 'uploads'
+OutDir = 'registered'
+flist = getFileNameList(InputDir, '*.xml')
+
+
+doBatchRegistration(OutDir, warpMarkers= True, includeMarkers=True, applyMask=False, TemplateIndex=1)
+
+# def register_images(request):
+# 	# if image exists:
+# 	# 	a = autoRegister(image)
+
+# 	# return warp(a)
